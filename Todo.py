31939cc4d6c1f8dc821abf893258a92bf102d323
mhunterak <Treehouse@branch.private>
2019-01-05 20:02:48 -0800

    updated Todo.py
    imported project dukes

--- Todo.py
+++ Todo.py
@@ -1,25 +1,70 @@
 '''
 MASTER TODO LIST for User Profile project
 '''
-#TODO: Use the supplied HTML/CSS to build and style the profile page and bio page.
+# TODO: Use the supplied HTML/CSS to build and style the
+# profile page and bio page.
 
-#TODO: Create a Django model for the user profile.
-#TODO: Add routes to display a profile, edit a profile, and change the password.
+# DONE: Create a Django model for the user profile.
+# TODO: Add routes to display a profile,
+# edit a profile,
+# and change the password.
 
-#TODO: Create a “profile” view to display a user profile with the following fields: First Name, Last Name, Email, Date of Birth, Bio and Avatar. Include a link to edit the profile.
-#TODO: Create an “edit” view with the route “/profile/edit” that allows the user to edit the user profile with the following fields: First Name, Last Name, Email, Date of Birth, Confirm Email, Bio and Avatar.
+# TODO: Create a “profile” view to display a user profile
+# with the following fields:
+# First Name,
+# Last Name,
+# Email,
+# Date of Birth,
+# Bio and
+# Avatar
+# Include a link to edit the profile.
 
-#TODO: Validate user input "Date of Birth" field: check for a proper date format (YYYY-MM-DD, MM/DD/YYYY, or MM/DD/YY)
-#TODO: Validate user input "Email" field: check that the email addresses match and are in a valid format.
-#TODO: Validate user input "Bio" field: check that the bio is 10 characters or longer and properly escapes HTML formatting.
+# TODO: Create an “edit” view with the route “/profile/edit”
+# that allows the user to edit the user profile with the following fields:
+# First Name,
+# Last Name,
+# Email,
+# Date of Birth,
+# Confirm Email,
+# Bio and
+# Avatar.
 
-#TODO: Add the ability to upload and save a user’s avatar image.
+# TODO: Validate user input "Date of Birth" field: 
+# check for a proper date format (YYYY-MM-DD, MM/DD/YYYY, or MM/DD/YY)
+# TODO: Validate user input "Email" field: check that
+# the email addresses match and are in a valid format.
+# TODO: Validate user input "Bio" field: check that the bio is
+# 10 characters or longer and properly escapes HTML formatting.
 
-#TODO: Create “change-password” view with the route “/profile/change_password” that allows the user to update their password using User.set_password() and then User.save(). Form fields will be: current password, new password, confirm password
-##TODO: Validate user input "Password" fields: check that the old password is correct using User.check_password() and the new password matches the confirm password field and follows the following password policy.
-##TODO: must not be the same as the current password minimum password length of 14 characters.
-##TODO: must use of both uppercase and lowercase letters must include one or more numerical digits must include one or more of special characters, such as @, #, $ cannot contain the user name or parts of the user’s full name, such as their first name
+# TODO: Add the ability to upload and save a user’s avatar image.
 
-#TODO: Use CSS to style headings, font, and form.
-#TODO: Make sure your coding style complies with PEP 8.
+# TODO: Create “change-password” view with the route
+# “/profile/change_password” that allows the user to update their password
+# using User.set_password() and then User.save(). Form fields will be:
+# current password, new password, confirm password
+# TODO: Validate user input "Password" fields: check that the old
+# password is correct using User.check_password() and the
+# new password matches the confirm password field and
+# follows the following password policy:
+# TODO: must not be the same as the current password
+# PROG: minimum password length of 14 characters
+# TODO: must use of both uppercase and lowercase letters
+# TODO: must include one or more numerical digits
+# TODO: must include one or more of special characters, such as @, # , $
+# TODO: cannot contain the user name or parts of the user’s full name,
+# such as their first name
 
+# TODO: Use CSS to style headings, font, and form.
+# TODO: Make sure your coding style complies with PEP 8.
+
+# EXTRA CREDIT:
+# TODO: Add additional form fields to build a more complex form
+# with additional options, such as city/state/country of residence,
+# favorite animal or hobby,
+# TODO: JavaScript is utilized for a date dropdown for
+# the Date of Birth validation feature.
+# TODO: JavaScript is utilized for text formatting for
+# the Bio validation feature.
+# TODO: Add an online image editor to the avatar.
+# Include the basic functionality: rotate, crop and flip. PNG mockup supplied.
+# TODO: A password strength “meter” is displayed when validating passwords.
---
+++ profile_project/accounts/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+
+# INPROG: Create a Django model for the user profile.
+class Account(models.Model):
+    '''
+Account is the main class for this project, and stores user information to be
+displayed on their profile page. Does not handle authentication directly,
+    '''
+    # First Name
+    firstname = models.CharField(
+        max_length=64,
+    )
+    # Last Name
+    lastname = models.CharField(
+        max_length=64,
+    )
+    # Email - also the primary key
+    enail = models.EmailField(
+        unique=True,
+    )
+    # Function for disabling bad actors
+    is_active = models.BooleanField(default=True)
+    # Password
+    password = models.CharField(
+        max_length=256,
+    )
+    # Date of Birth
+    dob = models.DateField()
+    # Bio
+    '''The bio validation should check that the bio is 10 characters or longer
+    and properly escapes HTML formatting.'''
+    bio = models.TextField(
+        max_length=16384,
+    )
+    # Avatar
+    avatar = models.ImageField()
